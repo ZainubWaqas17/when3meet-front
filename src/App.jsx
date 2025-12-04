ca72a083e52ffcfd037506ba9799611b1e366ec9
Sarang Kale <sarangkale04@gmail.com>
2025-12-03 22:31:35 -0500

    Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+// src/App.jsx
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import Home from './pages/Home.jsx'
-import Availability from './pages/Availability.jsx'
-import CreateEvent from './pages/CreateEvent.jsx'
-import SaveMeeting from './pages/SaveMeeting.jsx'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import CreateEvent from './pages/CreateEvent'
+import Availability from './pages/Availability'
+import SaveMeeting from './pages/SaveMeeting'
+import Register from './pages/Register'
 
-export default function App() {
+function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
       <Route path="/availability" element={<Availability />} />
       <Route path="/create" element={<CreateEvent />} />
-      <Route path="/event/:eventId/summary" element={<SaveMeeting />} /> {/* After meeting sceduled have this page*/}
+      <Route path="/event/:eventId/summary" element={<SaveMeeting />} />
       <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   )
 }
+
+export default App
